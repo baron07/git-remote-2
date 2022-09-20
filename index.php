4fac72a28fc53c0cb30d598feee9817303ec3685
Ainun Fakhri <kiplifakhri@gmail.com>
2022-09-20 21:05:00 +0700

    Menambahkan list

--- index.php
+++ index.php
@@ -6,5 +6,9 @@
     <body>
         <h1>Hello World!</h1>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum, minus fugit neque esse ducimus animi impedit in rerum. Ex repudiandae autem et qui debitis, quibusdam repellendus ipsa earum quod molestiae.</p>
+        <ul>
+            <li>Satu</li>
+            <li>Dua</li>
+        </ul>
     </body>
 </html>
